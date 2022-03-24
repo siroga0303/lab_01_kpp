028930c2ef83e748166b49484a291068a0ba7ab7
Serhio <siroga.03.03@gmail.com>
2022-03-24 12:39:58 +0200

    FINAL VERSION

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomNumber() {
 }
 function computerNumber() {
 	let fourth = randomNumber();
-	if (fourth == 0) {
+	while (fourth == 0) {
 		fourth = randomNumber();
 	}
 	for (let i; i = 1; i++) {
@@ -16,7 +16,7 @@ function computerNumber() {
 		let first = randomNumber();
 		if (third != fourth && second != fourth && second != third && first != fourth && first != third && first != second) {
 			let rnd = fourth.toString() + third.toString() + second.toString() + first.toString();
-			console.log(rnd);
+			
 			return rnd;
 		}
 	}
@@ -25,16 +25,24 @@ function computerNumber() {
 const secretNumber = parseInt(computerNumber());
 console.log(secretNumber);
 const length = String(secretNumber).length;
+
 console.log(`Загадане число містить ${length} цифри`);
 
 //function compare the generated and input numbers
-
 const getHint = (secret,guess) => {
 	let bulls = 0;
 	let cows = 0;
-    if(guess.length > 4 || guess.length < 4){
-        return `4 цифри потірбно, у вас ${guess.length}`;
-    }
+	let playerNumber = parseInt(guess)
+	if (!parseInt(guess)) {
+		return 'Введіть число'
+	}
+	if (guess[0] == 0) {
+		return 'Число не може розпочинатись із "0"'
+	}
+	if (guess.length > 4 || guess.length < 4) {
+		return `4 цифри потірбно, у вас ${guess.length}`;
+	}
+
 	const secretArr = String(secret).split('').map(elem => +elem);
 	const guessArr = guess.split('').map(elem => +elem);
 
@@ -47,7 +55,6 @@ const getHint = (secret,guess) => {
 		else i += 1;
 	}
 
-
 	for (let j = 0; j <= guessArr.length; j++) {
 		if (secretArr.includes(guessArr[j])) {
 			cows += 1;
@@ -55,17 +62,7 @@ const getHint = (secret,guess) => {
 		}
 	}
 
-
-for (let j = 0; j <= guessArr.length; j++) {
-    if (secretArr.includes(guessArr[j])) {
-        cows += 1;
-        secretArr.splice(secretArr.indexOf(guessArr[j]), 1);
-    }
-}
-
-    return `Бики: ${bulls}; Корови: ${cows}.`;
-
-
+	return `Бики: ${bulls}; Корови: ${cows}.`;
 };
 
 //start the game!
