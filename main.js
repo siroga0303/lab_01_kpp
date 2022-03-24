17f34c06dcd32f5eef8e51f76a2a002809ebeb59
Serhio <siroga.03.03@gmail.com>
2022-03-24 12:22:52 +0200

    delete name player

--- main.js
+++ main.js
@@ -1,70 +1,65 @@
 const readLine = require('readline-sync');
-
 //generate the random number
-function randomNumber() {
-	var number = Math.floor(Math.random() * 10);
-	return number
-}
-function computerNumber() {
-	var fourth = randomNumber();
-	for (var i; i = 1; i++) {
-		var third = randomNumber();
-		var second = randomNumber();
-		var first = randomNumber();
-		if (third != fourth && second != fourth && second != third && first != fourth && first != third && first != second) {
-			var rnd = fourth.toString() + third.toString() + second.toString() + first.toString();
-			console.log(rnd);
-			return rnd;
-		}
-	}
-
-}
-
-let name = readLine.question('What is your name? ');
-
-while (name === '') {
-	name = readLine.question('What is your name? ')
-}
-
+function randomNumber(){
+    let number= Math.floor(Math.random()*10);
+    return number
+    }
+function computerNumber(){
+        let fourth= randomNumber();
+        if(fourth == 0){
+            fourth= randomNumber();
+        }
+        for (let i;i=1;i++) {
+            let third= randomNumber();
+            let second= randomNumber();
+            let first= randomNumber();
+            if(third!=fourth && second!=fourth && second!=third && first!=fourth && first!=third && first!=second){
+                let rnd=fourth.toString()+third.toString()+second.toString()+first.toString();
+                console.log(rnd);
+        return rnd;
+            }
+        }
+        
+    }
 const secretNumber = parseInt(computerNumber());
 console.log(secretNumber);
 const length = String(secretNumber).length;
 console.log(`My guessed number contains ${length} digits`);
 
 //function compare the generated and input numbers
-const getHint = (secret,guess) => {
+const getHint = (secret, guess) => {
 
-	let bulls = 0;
-	let cows = 0;
-	const secretArr = String(secret).split('').map(elem => +elem);
-	const guessArr = guess.split('').map(elem => +elem);
+let bulls = 0;
+let cows = 0;
+const secretArr = String(secret).split('').map(elem => +elem);
+const guessArr = guess.split('').map(elem => +elem);
 
-	for (let i = 0; i < secretArr.length;) {
-		if (secretArr[i] === guessArr[i]) {
-			bulls += 1;
-			secretArr.splice(i,1);
-			guessArr.splice(i,1);
-		}
-		else i += 1;
-	}
+for (let i = 0; i < secretArr.length;) {
+    if (secretArr[i] === guessArr[i]) {
+        bulls += 1;
+        secretArr.splice(i, 1);
+        guessArr.splice(i, 1);
+    }
+    else i += 1;
+}
 
-	for (let j = 0; j <= guessArr.length; j++) {
-		if (secretArr.includes(guessArr[j])) {
-			cows += 1;
-			secretArr.splice(secretArr.indexOf(guessArr[j]),1);
-		}
-	}
-	return `in your number ${bulls} bulls and ${cows} cows`;
+for (let j = 0; j <= guessArr.length; j++) {
+    if (secretArr.includes(guessArr[j])) {
+        cows += 1;
+        secretArr.splice(secretArr.indexOf(guessArr[j]), 1);
+    }
+}
+    return `У вашому числі ${bulls} биків та ${cows} корів`;
 };
 
 //start the game!
 while (true) {
 
-	const query = readLine.question("input your number: ");
-	if (+query === secretNumber) {
-		console.log(`${name} win! Number what i guess is really ${secretNumber}`);
-		break;
-	}
+const query = readLine.question("Уведіть ваше число: ");
+  if (+query === secretNumber) {
+    console.log(`Ви виграли це і правда число ${secretNumber}`);
+    break;
+  }
 
-	console.log(`${getHint(secretNumber,query)}`);
+console.log(`${getHint(secretNumber, query)}`);
 };
